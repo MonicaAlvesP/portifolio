cb42fe4c788a6b2e18e69e5340c02c8ba8fd1d78
MonicaAlvesP <monica52782@gmail.com>
2024-04-01 18:21:01 -0300

    Alterações feitas na pagina Sobre

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,9 +1,9 @@
-import React from "react"
+import React from "react";
 
 export default function Projetos() {
-    return (
+    return(
         <>
-            <h2>Projetos</h2>
+            <h1>Eu sou Projetos!</h1>
         </>
     )
 }
--- src/components/Style.jsx
+++ src/components/Style.jsx
@@ -31,10 +31,11 @@ export const Section = styled.section`
     justify-content: center;
     margin: 2rem;
     width: 25%;
+    height: 45%;
     background-color: rgba(8, 38, 57, 0.9);
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
     color: aliceblue;
-    border-radius: 20px;
+    border-radius: 25px;
 
 h1{
     text-align: center;
@@ -45,16 +46,24 @@ h1{
 export const Div = styled.div`
     height: 20%;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-between;
     align-items: center;
     margin-bottom: 5%;
+    padding: 0 5vh;
 
 h3{
     font-family: "Quicksand", sans-serif;
     font-size: 1rem;
+@media only screen and (max-width: 600px) {
+    font-size: 0.8rem;
+}
 }
 `
 
 export const Imagens = styled.img`
     max-width: 7vh;
+
+@media only screen and (max-width: 600px) {
+    max-width: 5vh;
+}
 `
